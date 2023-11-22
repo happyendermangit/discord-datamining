7aa6bf9005eba550cd5c34abf09c3a1faf74f419
Happy enderman <66224387+happyendermangit@users.noreply.github.com>
2023-11-22 15:10:48 +0100

    Update buildHistory.py

--- src/buildHistory.py
+++ src/buildHistory.py
@@ -1,37 +1,56 @@
-import requests,os 
+import requests
+import os
+from github import Github
+
+
+def save_html(url, path):
+    response = requests.get(url)
+    build_id = response.headers.get('X-Build-Id')
+    html_content = response.text
+
+    os.mkdir(path + build_id)
+
+    with open(path + build_id + '/index.html', 'a+', encoding="utf-8") as f:
+        f.write(html_content)
+
+    return build_id 
+
+github_token = os.getenv('ACCESS_TOKEN')
+repository_name = 'discord-datamining'
+
+
+g = Github(github_token)
+repo = g.get_user().get_repo(repository_name)
 
 ptb_path = "./ptb/"
 canary_path = "./canary/"
 stable_path = "./stable/"
 
 
-ptb = requests.get('https://ptb.discord.com/app')
 
-canary = requests.get('https://canary.discord.com/app')
-
-stable = requests.get('https://discord.com/app')
-
-ptb = {"hash":ptb.headers['X-Build-Id'],"html":ptb.text}
-
-canary = {"hash":canary.headers['X-Build-Id'],"html":canary.text}
-
-stable = {"hash":stable.headers['X-Build-Id'],"html":stable.text}
-
-os.mkdir(ptb_path + ptb.get('hash'))
-
-with open(ptb_path + ptb.get('hash')+'/index.html','a+',encoding="utf-8") as f:
-    f.write(ptb.get('html'))
+ptb = save_html('https://ptb.discord.com/app', ptb_path)
 
 
-os.mkdir(canary_path + canary.get('hash'))
-
-with open(canary_path + canary.get('hash')+'/index.html','a+',encoding="utf-8") as f:
-    f.write(canary.get('html'))
+canary = save_html('https://canary.discord.com/app', canary_path)
 
 
-os.mkdir(stable_path + stable.get('hash'))
+stable = save_html('https://discord.com/app', stable_path)
 
-with open(stable_path + stable.get('hash')+'/index.html','a+',encoding="utf-8") as f:
-    f.write(stable.get('html'))
+ptb_commit_msg = f"✅ PTB builds updated ({ptb})"
+canary_commit_msg = f"✅ Canary builds updated ({canary})"
+stable_commit_msg = f"✅ Stable builds updated ({stable})"
 
 
+branch_name = "main"  
+
+
+ptb_commit = repo.get_branch(branch_name).commit
+ptb_commit.create_comment(ptb_commit_msg)
+
+
+canary_commit = repo.get_branch(branch_name).commit
+canary_commit.create_comment(canary_commit_msg)
+
+
+stable_commit = repo.get_branch(branch_name).commit
+stable_commit.create_comment(stable_commit_msg)

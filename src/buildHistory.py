f3d41e7244917a79914eaff2b163e09273336445
Happy enderman <66224387+happyendermangit@users.noreply.github.com>
2023-11-23 18:46:17 +0100

    Update buildHistory.py

--- src/buildHistory.py
+++ src/buildHistory.py
@@ -1,15 +1,36 @@
 import requests
 import os
+import json 
 from github import Github
+from datetime import datetime
+
+builds = requests.get("https://raw.githubusercontent.com/happyendermangit/discord-datamining/main/strings.json").json()
+
+
+
+class Build:
+    def __init__(self,hash,type,date,html) -> None:
+        self.hash = hash 
+        self.type = type 
+        self.date = date 
+        self.html = html
 
 def save_html(url, path):
     response = requests.get(url)
     build_id = response.headers.get('X-Build-Id')
     html_content = response.text
     if os.path.isdir(path + "/"+build_id) == False:
+        
         os.mkdir(path + "/"+build_id)
+        current_datetime = datetime.now()
+        date_now = current_datetime.strftime("%A, %B %Y,%w %H:%M:%S")
+
+        b = Build(build_id,path.split('/')[1],date_now,html_content).__dict__
+        builds.push(b)
         with open(path + "/"+build_id+"/" +f'index.html', 'a+', encoding="utf-8") as f:
             f.write(html_content)
+        with open(path + "/"+build_id+"/" +"build.json", 'a+') as json_file:
+            json.dump(b, json_file, indent=2)
 
     return build_id 
 
@@ -34,6 +55,8 @@ canary = save_html('https://canary.discord.com/app', canary_path)
 
 stable = save_html('https://discord.com/app', stable_path)
 
+with open("./builds.json", 'w') as json_file:
+    json.dump(builds, json_file, indent=2)
 
 branch_name = "main"  
 
